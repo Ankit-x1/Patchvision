044e8d41b2e6d9e6033298d7efaeb64c4aae862d
Ankitkarki <karkiankit101@gmail.com>
2025-12-25 00:31:14 -0600

    Package initiated

---
+++ patchvision/__init__.py
@@ -0,0 +1,18 @@
+"""
+PatchVision - Industrial-Grade Vision Processing Framework
+
+Production-ready computer vision for industrial applications.
+"""
+
+from .core import PatchFactory, TokenProjector, InferenceEngine
+
+__version__ = "1.0.0"
+__author__ = "Ankit Karki"
+__email__ = "karkiankit101@gmail.com"
+
+__all__ = [
+    'PatchFactory', 
+    'TokenProjector', 
+    'InferenceEngine',
+    '__version__'
+]
